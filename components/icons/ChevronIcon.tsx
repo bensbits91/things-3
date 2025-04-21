389137526594b2c00feba9b3d62f089faf944f62
bensbits91 <benbpnw@gmail.com>
2025-04-20 18:21:05 -0700

    fix typo in icon css class

--- components/icons/ChevronIcon.tsx
+++ components/icons/ChevronIcon.tsx
@@ -2,7 +2,7 @@ const ChevronIcon = ({ direction = 'down' }) => (
    <div
       className={`${
          direction === 'up' ? 'rotate-180 ' : ''
-      }transition-transform trasition-duration-500`}>
+      }transition-transform transition-duration-500`}>
       <svg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
          <path
             d='M17 9.5L12 14.5L7 9.5'
